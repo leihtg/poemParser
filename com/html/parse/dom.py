717701f130389978bd48a877c6943c4a164e21ab
leihtg <leihtg@163.com>
2018-05-08 17:39:20 +0800

    complex

--- com/html/parse/dom.py
+++ com/html/parse/dom.py
@@ -22,25 +22,32 @@ class Dom:
     def setText(self, text):
         self.text += text
 
+    def attr(self, name):
+        for a in self.attrs:
+            if a[0] == name:
+                return a[1]
+
     def hasAttr(self, name, value):
         for a in self.attrs:
             if name == a[0] and value == a[1]:
                 return True
         return False
 
-    def __find(self, selector=""):
-        if "#" == selector[:1]:
-            if self.hasAttr('id', selector[1:]):
-                return self
-            else:
-                for c in self.children:
-                    dest = c.__find(selector)
-                    if dest:
-                        return dest
-            return None
-        return None
+    def __find(self, an, av, q=[]):
+        if self.hasAttr(an, av):
+            q.append(self)
+        else:
+            for c in self.children:
+                c.__find(an, av, q)
+        return q
 
     def find(self, selector=""):
+        q = []
         if selector == "":
-            return None
-        return self.__find(selector)
+            return q
+        if "#" == selector[:1]:
+            return self.__find('id', selector[1:], q)
+        elif "." == selector[:1]:
+            return self.__find('class', selector[1:], q)
+
+        return q
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from com.html.parse import *
 import time as t
 
 url = 'https://so.gushiwen.org/authors/authorvsw_3b99a16ff2ddA01.aspx'
-# url = 'http://www.xuebuyuan.com/1880339.html'
+url = 'https://so.gushiwen.org/authors/'
 # data = open(r'C:\Users\Thinkpad\Desktop\a.txt', encoding="utf-8").read()
 start = t.time()
 a = HtmlDomParser(url)
@@ -13,6 +13,11 @@ print("parser cost(s) %f" % (t.time() - start))
 
 start = t.time()
 rd = a.getDom()
-print(rd.find('#b'))
+f=rd.find('#txtare71144')
+f=rd.find('.pages')
+for e in f:
+   print(e,e.text)
+   for c in e.children:
+      print(c,c.text,c.attr('href'))
 print("find dom cost(s) %f" % (t.time() - start))
 
