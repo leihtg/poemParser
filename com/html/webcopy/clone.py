e84a10accdfe400613169632815f4f83740e01a2
leihtg <leihtg@163.com>
2018-06-25 22:44:37 +0800

    9 , 2018/06/25 周一 22:44:36.74

--- com/html/webcopy/clone.py
+++ com/html/webcopy/clone.py
@@ -1,15 +1,10 @@
 from com.html.parse import *
 import os
 
-path = r"C:\Users\leihuating\Desktop\hehe\a.html"
-print(os.path.exists(path))
-os.makedirs(path)
-
 
 class CopyWeb:
-    def __init__(self, savePath, context, url):
+    def __init__(self, savePath, url):
         self.savePath = savePath
-        self.context = context
         self.url = url
         pass
 
@@ -18,22 +13,29 @@ class CopyWeb:
         return (('link', 'href'), ('script', 'src'), ('a', 'href'), ('iframe', 'src'))
 
     def doCopy(self):
-        urls=[]
+        urls = []
         parser = HtmlDomParser(self.url)
         dom = parser.parse()
         jq = jQuery(dom)
         fname = jq.find('title').text()
         for tr in self.__transfer():
+            for f in jq.find(tr[0]):
+                u = f.attr(tr[1])
+                if u and u[0] == '/':
+                    f.attr(tr[1], '.%s' % u)
+                    urls.append((parser.getRealUrl(u), u))
             pass
-
-        pass
+        self.__write(fname, dom.toHtml())
 
     def __write(self, fname, data):
         if not os.path.exists(self.savePath):
             os.makedirs(self.savePath)
-        f = open(fname, mode='w')
+        f = open('%s/%s.html' % (self.savePath, fname), mode='w',encoding='utf8')
         f.write(data)
 
+
+cw = CopyWeb(r'C:\Users\Thinkpad\Desktop\copy', 'https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_18119813560590364103%22%7D&n_type=0&p_from=1')
+cw.doCopy()
 
 from redis import *
 
