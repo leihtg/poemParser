4687b05de0b0a8011563eee3bbd741a79355d489
leihtg <leihtg@163.com>
2020-08-26 10:04:26 +0800

    添加算术编码算法

--- com/use/persist/mysql.py
+++ com/use/persist/mysql.py
@@ -1,4 +1,7 @@
+import random
+
 import pymysql as mysql
+import redis as redis
 
 
 class Mysql:
@@ -39,22 +42,17 @@ class Mysql:
         self.conn.commit()
 
 
-if __name__ == "__main__":
-    import redis
-    import time as tt
-
+def savePoemToDb():
+    global conn
     conn = Mysql("172.29.97.155", "root", "root", "springboot", charset="utf8")
-
 
     def saveAuthor(id, name):
         conn.exec("insert into Author(id,name) values('%d','%s')" % (id, name))
-
 
     def savePoem(id, title, authorId, content=""):
         content = content.replace("'", "''")
         conn.exec(
             "insert into Poem(id,title,authorId,content) values ('%d','%s','%s','%s')" % (id, title, authorId, content))
-
 
     start = tt.time()
     r = redis.Redis(host="localhost", port=6379)
@@ -75,5 +73,33 @@ if __name__ == "__main__":
                 content = data[:pos]
                 savePoem(t, title, count, content)
             conn.commit()
-
     print('cost (s) %f' % (tt.time() - start))
+
+
+def insertAccount():
+    conn = Mysql("101.37.19.131", "root", "Dayee@1603", "oivib_test", port=3308, charset="utf8")
+    for i in range(1, 50000):
+        interviewId = int(i / 5 + 1)
+        accountId = ('%05d' % i)
+        role = [0, 1][random.randint(0, 1)]
+        t = tt.strftime("%Y-%m-%d %H:%M:%S")
+        # conn.exec(
+        #     "insert into `t_interview_account` (`F_INTERVIEW_ID`, `F_ACCOUNT_ID`, `F_ROLE`, `F_UID`, `f_post_name`, `F_USER_NAME`, `F_ADD_TIME`, `F_UPDATE_TIME`) values('%s','%s','%s','%s','%s','%s','%s','%s');" % (
+        #         interviewId, accountId, role, i, '测试', '张%s' % i, t, t))
+        try:
+            conn.exec(
+                "insert into `t_interview_plan` (`f_id`, `f_corp_code`, `f_corp_name`, `f_start_time`, `f_end_time`, `f_mode`, `f_add_time`, `f_update_time`) values('%s','dayeesz','大易测试',now(),'2020-03-15 18:06:26','2',now(),now());" %
+                interviewId)
+        except:
+            pass
+        if (i % 1000 == 0):
+            conn.commit()
+        print(i)
+
+
+if __name__ == "__main__":
+    # import redis
+    import time as tt
+
+    # savePoemToDb()
+    insertAccount()
